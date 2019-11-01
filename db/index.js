bd0937973c15c4a24e0a9377e4724c9ebf105717
Michael Scanza <mscanza@gmail.com>
2019-11-01 15:41:41 -0700

    Begin writing sql script

--- db/index.js
+++ db/index.js
@@ -1,125 +1,154 @@
-const mongoose = require('mongoose');
-const Randomly = require('./randomGenerate');
-const autoIncrement = require('mongoose-auto-increment');
-const mongodbUrl = 'mongodb://localhost:27017/cr_database';
-var connection = mongoose.createConnection(mongodbUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+const { Client } = require('pg')
+const client = new Client({
+  database: 'mypostgresdb'
+})
 
-autoIncrement.initialize(connection);
-// const db = mongoose.connection;
+const connection = async () => {
+  await client.connect()
+const res = await client.query("SELECT * from mypostgrestable");
+console.log(res.rows) // Hello world!
+await client.end()
+}
 
-// // eslint-disable-next-line no-console
-// db.on('error', console.error.bind(console, 'connection err'));
-// db.once('open', () => {
-//   autoIncrement.initialize(db);
-//   'Connection succeeded.';
+connection();
+// const mongoose = require('mongoose');
+// const Randomly = require('./randomGenerate');
+// const autoIncrement = require('mongoose-auto-increment');
+// const mongodbUrl = 'mongodb://localhost:27017/cr_database';
+// var connection = mongoose.createConnection(mongodbUrl, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
 // });
 
-const rvSchema = mongoose.Schema({
-  movie_name: String,
-  review: String,
-  rate: String,
-  rank: String,
-  publication: String,
-  date_post: String,
-});
+// autoIncrement.initialize(connection);
+// // const db = mongoose.connection;
 
-const crSchema = mongoose.Schema({
-  user_name: String,
-  user_photo: String,
-  user_page: String,
-  reviews: rvSchema,
-});
+// // // eslint-disable-next-line no-console
+// // db.on('error', console.error.bind(console, 'connection err'));
+// // db.once('open', () => {
+// //   autoIncrement.initialize(db);
+// //   'Connection succeeded.';
+// // });
 
-crSchema.plugin(autoIncrement.plugin, 'CReview')
+// const rvSchema = mongoose.Schema({
+//   movie_name: String,
+//   review: String,
+//   rate: String,
+//   rank: String,
+//   publication: String,
+//   date_post: String,
+// });
 
-const CReviews = connection.model('CReviews', rvSchema);
-const CReview = connection.model('CReview', crSchema);
+// const crSchema = mongoose.Schema({
+//   id: Number,
+//   user_name: String,
+//   user_photo: String,
+//   user_page: String,
+//   reviews: rvSchema,
+// });
 
-//Create a unique id
-const saveCReviews = ((creviews) => {
-  creviews.forEach((review) => {
-    const data = review.reviews.date_post || Randomly.RandomDate();
-    const name = review.user_name || Randomly.RandomName();
-    const rate = review.reviews.rate || Randomly.RandomRate();
-    const movie = review.reviews.movie_name || Randomly.RandomMovie();
-    const rank = review.reviews.rank || Randomly.RandomRank();
-    const photo = review.user_photo || Randomly.RandomPhoto();
-    const publication = review.reviews.publication || Randomly.RandomPublication();
+// crSchema.plugin(autoIncrement.plugin, 'CReview')
 
-    const crReviews = new CReviews({
-      movie_name: movie,
-      review: review.reviews.review,
-      rate,
-      rank,
-      publication,
-      date_post: data,
-    });
+// const CReviews = connection.model('CReviews', rvSchema);
+// const CReview = connection.model('CReview', crSchema);
 
-    const crReview = new CReview({
-      user_name: name,
-      user_photo: photo,
-      user_page: review.user_page,
-      reviews: crReviews,
-    });
 
-    crReview.save((err) => {
-      if (err) throw err;
-    });
-  });
-});
+// let documentArray = [];
+// let idIndex = 100;
+// const saveCReviews = ((creviews) => {
+//   creviews.forEach((review) => {
+//     const data = review.reviews.date_post || Randomly.RandomDate();
+//     const name = review.user_name || Randomly.RandomName();
+//     const rate = review.reviews.rate || Randomly.RandomRate();
+//     const movie = review.reviews.movie_name || Randomly.RandomMovie();
+//     const rank = review.reviews.rank || Randomly.RandomRank();
+//     const photo = review.user_photo || Randomly.RandomPhoto();
+//     const publication = review.reviews.publication || Randomly.RandomPublication();
 
-const getCReviews = (callback) => {
-  CReview.find({})
-    .sort({ 'CReview.reviews.date_post': -1 })
-    .exec((err, results) => {
-      if (err) callback(err, null);
-      callback(null, results);
-    });
-};
+//     const crReviews = new CReviews({
+//       movie_name: movie,
+//       review: review.reviews.review,
+//       rate,
+//       rank,
+//       publication,
+//       date_post: data,
+//     });
 
-const getCReviewsById = (id, callback) => {
-  CReview.find({_id: id}, (err, doc) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, doc)
-    }
-  })
-}
+//     const crReview = new CReview({
+//       id: idIndex,
+//       user_name: name,
+//       user_photo: photo,
+//       user_page: review.user_page,
+//       reviews: crReviews,
+//     });
 
-const postNewReview = (data) => {
-    saveCReviews([data]);
-}
+//     documentArray.push(crReview);
+//     idIndex++;
+//   });
+// });
 
-const updateReview = (id, query, callback) => {
-  CReview.findOneAndUpdate({_id: id}, query, (err, doc) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, doc);
-    }
-  })
-}
+// const seedDB = (arr, callback) => {
+//   CReview.insertMany(arr, (err, docs) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(`${arr.length} documents seeded.`)
+//       callback()
+//     }
+//   })
+// }
 
-const deleteReview = (id, callback) => {
-  CReview.remove({_id: id}, (err, doc) => {
-    if (err) {
-      callback(err);
-    } else {
-      callback(null, doc);
-    }
-  })
-}
+// const getCReviews = (callback) => {
+//   CReview.find({})
+//     .sort({ 'CReview.reviews.date_post': -1 })
+//     .exec((err, results) => {
+//       if (err) callback(err, null);
+//       callback(null, results);
+//     });
+// };
 
-module.exports = {
-  CReview,
-  saveCReviews,
-  getCReviews,
-  getCReviewsById,
-  postNewReview,
-  deleteReview,
-  updateReview
-};
+// const getCReviewsById = (id, callback) => {
+//   CReview.find({_id: id}, (err, doc) => {
+//     if (err) {
+//       callback(err);
+//     } else {
+//       callback(null, doc)
+//     }
+//   })
+// }
+
+// const postNewReview = (data) => {
+//     saveCReviews([data]);
+// }
+
+// const updateReview = (id, query, callback) => {
+//   CReview.findOneAndUpdate({_id: id}, query, (err, doc) => {
+//     if (err) {
+//       callback(err);
+//     } else {
+//       callback(null, doc);
+//     }
+//   })
+// }
+
+// const deleteReview = (id, callback) => {
+//   CReview.remove({_id: id}, (err, doc) => {
+//     if (err) {
+//       callback(err);
+//     } else {
+//       callback(null, doc);
+//     }
+//   })
+// }
+
+// module.exports = {
+//   documentArray,
+//   seedDB,
+//   CReview,
+//   saveCReviews,
+//   getCReviews,
+//   getCReviewsById,
+//   postNewReview,
+//   deleteReview,
+//   updateReview
+// };
--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,17 @@
 const db = require('.');
 // eslint-disable-next-line camelcase
+
+//pushes into documentArray
 const dummy_data = require('./dummy-data.json');
-for (let i = 0; i < 10; i++) {
+for (let i = 0; i < 8000; i++) {
   db.saveCReviews(dummy_data);
 }
+db.seedDB(db.documentArray, () => {
+  for (let i = 0; i < 8000; i++) {
+    db.saveCReviews(dummy_data);
+  }
+  db.seedDB(db.documentArray, () => {
+    console.log('done!')
+  })
+})
+

fbf54d5658c445d4fa34c86e09565acc7eff7344
Michael Scanza <mscanza@gmail.com>
2019-11-04 13:35:08 -0800

    Refactor to create separate CSV files for reviews and critics

--- db/generateCSVfiles.js
+++ db/generateCSVfiles.js
@@ -1,40 +1,74 @@
 const faker = require('faker');
 const Randomly = require('./randomGenerate.js');
-
+const generateCritics = require('./criticsGenerate').generateCritics
 const csvWriter = require('csv-write-stream');
 const fs = require('fs');
-const writer = csvWriter({
-  headers: ['user_name', 'user_photo','user_page','movie_name','review','rate','rank','publication']
-});
-
-writer.pipe(fs.createWriteStream('largeData.csv'))
 
 let data;
-function writeTenMillionTimes(writer, data, callback) {
+let critics = [];
+generateCritics(critics);
+
+function writeCriticsCSV(writer, data, callback) {
+  let i = critics.length - 1;
+  write();
+  function write() {
+    let ok = true;
+    do {
+      data = critics[i];
+      i -= 1;
+      if (i === 0) {
+        writer.write(data, callback);
+      } else {
+        ok = writer.write(data);
+      }
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+}
+
+function writeReviewsCSV(writer, data, callback) {
   let i = 10000000;
   write();
   function write() {
     let ok = true;
     do {
-      data = [Randomly.RandomName(), Randomly.RandomPhoto(),faker.internet.domainName(),Randomly.RandomMovie(),faker.lorem.paragraph(),Randomly.RandomRate(),Randomly.RandomRank(),Randomly.RandomPublication()];
+      data = [Randomly.RandomMovie(),faker.lorem.paragraph(),Randomly.RandomRate(),Randomly.RandomRank(),Randomly.RandomPublication(), Randomly.RandomReviewerId()];
       i -= 1;
       if (i === 0) {
-        // Last time!
         writer.write(data, callback);
       } else {
-        // See if we should continue, or wait.
-        // Don't pass the callback, because we're not done yet.
         ok = writer.write(data);
       }
     } while (i > 0 && ok);
     if (i > 0) {
-      // Had to stop early!
-      // Write some more once it drains.
       writer.once('drain', write);
     }
   }
 }
-writeTenMillionTimes(writer, data, () => {console.log('done!')})
+
+let writer = csvWriter({
+  headers: ['user_name', 'user_photo','user_page']
+})
+writer.pipe(fs.createWriteStream('criticData.csv'))
+
+writeCriticsCSV(writer, data, () => {
+  console.log('Critics CSV created!')
+})
+
+
+  writer = csvWriter({
+  headers: ['movie_name','review','rate','rank','publication', 'reviewer_id']
+});
+
+writer.pipe(fs.createWriteStream('reviewsData.csv'))
+
+writeReviewsCSV(writer, data, () => {
+  writer.end();
+  console.log('Reviews CSV created!')
+})
+
 
 // const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 // const csvWriter = createCsvWriter({

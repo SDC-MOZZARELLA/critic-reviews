b6dd6dab77d6891fbb29f6111bd686cd91bed4d3
Michael Scanza <mscanza@gmail.com>
2019-11-06 15:28:20 -0800

    Fix styling on index.js

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
+
+const cors = require('cors');
+
+// const db = require('../db');
+
 const app = express();
 const postgresRoutes = require('../db/postgresRoutes.js');
+
 const port = process.env.PORT || 4540;
 
-const db = require('../db');
-const cors = require('cors')
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
@@ -13,17 +18,16 @@ app.use(express.static('./public'));
 app.get('/api/cr_reviews/', (req, res) => {
   postgresRoutes.get100Reviews((err, docs) => {
     if (err) {
-      res.sendStatus(400)
+      res.sendStatus(400);
     } else {
-      res.send(docs)
+      res.send(docs);
     }
-  })
-    // Mongo
-    // db.getCReviews((err, results) => {
-    //   if (err) throw err;
-    //   res.send(results).end();
-    // });
-
+  });
+  // Mongo
+  // db.getCReviews((err, results) => {
+  //   if (err) throw err;
+  //   res.send(results).end();
+  // });
 });
 
 app.get('/api/cr_reviews/:id', (req, res) => {
@@ -33,41 +37,40 @@ app.get('/api/cr_reviews/:id', (req, res) => {
     } else {
       res.send(doc);
     }
-  })
-    // Mongo
-    // db.getCReviewsById(req.params.id, (err, doc) => {
-    //   if (err) {
-    //     res.sendStatus(404)
-    //   } else {
-    //     res.send(doc);
-    //   }
-    // })
-})
+  });
+  // Mongo
+  // db.getCReviewsById(req.params.id, (err, doc) => {
+  //   if (err) {
+  //     res.sendStatus(404)
+  //   } else {
+  //     res.send(doc);
+  //   }
+  // })
+});
 
 app.post('/api/cr_reviews/', (req, res) => {
   const { body } = req;
-  postgresRoutes.postReview(body, (err, result) => {
+  postgresRoutes.postReview(body, (err) => {
     if (err) {
-      res.send(err)
+      res.send(err);
     } else {
-      res.status(201).send('Thank you for your review!')
+      res.status(201).send('Thank you for your review!');
     }
-  })
+  });
   // Mongo
   // [data.user_name, data.user_photo, data.user_page]
   // db.postNewReview(req.body)
   // res.status(201).send('Review saved to database');
-})
+});
 
 app.put('/api/cr_reviews/:id', (req, res) => {
-  console.log(req.body)
-  postgresRoutes.updateReview(req.params.id, req.body, (err, success) => {
+  postgresRoutes.updateReview(req.params.id, req.body, (err) => {
     if (err) {
-      res.sendStatus(400)
+      res.sendStatus(400);
     } else {
-      res.status(202).send(`Successfully updated id: ${req.params.id}`)
+      res.status(202).send(`Successfully updated id: ${req.params.id}`);
     }
-  })
+  });
 
   // Mongo
   // db.updateReview(req.params.id, req.query, (err, doc) => {
@@ -77,17 +80,16 @@ app.put('/api/cr_reviews/:id', (req, res) => {
   //     res.status(202).send(`Successfully updated document id:${req.params.id}`)
   //   }
   // })
-})
+});
 
 app.delete('/api/cr_reviews/:id', (req, res) => {
-  postgresRoutes.deleteReview(req.params.id, (err, success) => {
+  postgresRoutes.deleteReview(req.params.id, (err) => {
     if (err) {
-      res.sendStatus(400)
+      res.sendStatus(400);
     } else {
-      res.status(202).send(`Successfully removed id: ${req.params.id}`)
+      res.status(202).send(`Successfully removed id: ${req.params.id}`);
     }
-  })
-
+  });
 
   // Mongo
   // db.deleteReview(req.params.id, (err, doc) => {
@@ -97,11 +99,7 @@ app.delete('/api/cr_reviews/:id', (req, res) => {
   //     res.status(202).send("Document successfully deleted")
   //   }
   // })
-})
-
-
-
-
+});
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
